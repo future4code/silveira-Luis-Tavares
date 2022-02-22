e2a3a5d1508e744eca30d7ff89a59ef6a8992208
luissfmt <luis_melotavares@hotmail.com>
2022-02-22 20:45:29 -0300

    Exercício Condicionais + Desafios Atualizado

--- modulo1/condicionais/index.js
+++ modulo1/condicionais/index.js
@@ -19,169 +19,175 @@ C) haverá erro no "console.log(mensagem)" pois a variável mensagem foi criada 
 */
 
 ////////////////////////////////////////////////////// 1 //////////////////////////////////////////////////////////
-// function podeDirigir(idade) {
-//     if(idade >= 18) 
-//         console.log("Você pode dirigir.");
-//      else 
-//         console.log("Você não pode dirigir.");
-// }
+function podeDirigir(idade) {
+    if(idade >= 18) 
+        console.log("Você pode dirigir.");
+     else 
+        console.log("Você não pode dirigir.");
+}
 
-// const idadeUsuario = +prompt("Qual a sua idade?");
+const idadeUsuario = +prompt("Qual a sua idade?");
 
-// podeDirigir(idadeUsuario);
+podeDirigir(idadeUsuario);
 
 // ////////////////////////////////////////////////////// 2 //////////////////////////////////////////////////////////
-// function turnoEstudo(turno) {
-//     if(turno.toLowerCase() === "m")
-//         console.log("Bom dia!")
-//      else if(turno.toLowerCase() === "v")
-//         console.log("Boa tarde!")
-//      else if(turno.toLowerCase() == "n")
-//         console.log("Boa noite!")
-//      else
-//         console.log("Favor digitar um turno válido.")
-// }
+function turnoEstudo(turno) {
+    if(turno.toLowerCase() === "m")
+        console.log("Bom dia!")
+     else if(turno.toLowerCase() === "v")
+        console.log("Boa tarde!")
+     else if(turno.toLowerCase() == "n")
+        console.log("Boa noite!")
+     else
+        console.log("Favor digitar um turno válido.")
+}
 
-// const turnoUsuario = prompt("Qual seu turno de estudo? (M = Matutino/V = Vespertino/N = Noturno)");
+const turnoUsuario = prompt("Qual seu turno de estudo? (M = Matutino/V = Vespertino/N = Noturno)");
 
-// turnoEstudo(turnoUsuario);
+turnoEstudo(turnoUsuario);
 
-// ////////////////////////////////////////////////////// 3 //////////////////////////////////////////////////////////
-// function turnoEstudo2(turno) {
-//     switch(turno.toLowerCase()) {
-//         case "m":
-//             console.log("Bom dia!")
-//             break;
+////////////////////////////////////////////////////// 3 //////////////////////////////////////////////////////////
+function turnoEstudo2(turno) {
+    switch(turno.toLowerCase()) {
+        case "m":
+            console.log("Bom dia!")
+            break;
 
-//         case "v":
-//             console.log("Boa tarde!")
-//             break;
+        case "v":
+            console.log("Boa tarde!")
+            break;
 
-//         case "n":
-//             console.log("Boa noite!")
-//             break;
+        case "n":
+            console.log("Boa noite!")
+            break;
 
-//         default:
-//             console.log("Favor digitar um turno válido.")
-//     }
-// }
+        default:
+            console.log("Favor digitar um turno válido.")
+    }
+}
 
-// turnoEstudo2(turnoUsuario); // Segui usando a mesmo valor da variável do prompt do exercício 2 (turnoUsuario).
+turnoEstudo2(turnoUsuario); // Segui usando a mesmo valor da variável do prompt do exercício 2 (turnoUsuario).
 
-// ////////////////////////////////////////////////////// 4 //////////////////////////////////////////////////////////
-// function escolhaFilme(genero, valorIng) {
-//     if(genero && valorIng) {
-//         const lanche = prompt("Qual lanche você vai comprar?") // prompt da tarefa do desáfio 1
-//         console.log("Bom filme!");        
-//         console.log(`Aproveite o seu/a sua ${lanche}!`);
-//     } else
-//         console.log("Escolha outro filme :(");
-// }
+////////////////////////////////////////////////////// 4 //////////////////////////////////////////////////////////
+function escolhaFilme(genero, valorIng) {
+    if(genero && valorIng) {
+        const lanche = prompt("Qual lanche você vai comprar?") // prompt da tarefa do desáfio 1
+        console.log("Bom filme!");        
+        console.log(`Aproveite o seu/a sua ${lanche}!`);
+    } else
+        console.log("Escolha outro filme :(");
+}
 
-// const generoFilme = prompt("Qual o gênero de filme será assistido?").toLowerCase() == "fantasia";
-// const valorIng = +prompt("Qual o valor do ingresso?") < 15;
+const generoFilme = prompt("Qual o gênero de filme será assistido?").toLowerCase() == "fantasia";
+const valorIng = +prompt("Qual o valor do ingresso?") < 15;
 
-// escolhaFilme(generoFilme, valorIng);
+escolhaFilme(generoFilme, valorIng);
 
 ////////////////////////////////////////////////////// DESAFIOS //////////////////////////////////////////////////////////
 // 2: 
 
-const nomeUsuario = prompt("Digite seu nome completo:");
-const tipoJogoUsuario = prompt("Qual o tipo de jogo? (IN = Internacional/DO = Doméstico)").toLocaleLowerCase();
-const etapaJogoUsuario = prompt("Qual a etapa do jogo? (SF = Semi-Final/DT = Decisão terceiro lugar/FI = Final").toLocaleLowerCase();
-const categoriaUsuario = +prompt("Qual a categoria? (1, 2, 3 ou 4)");
-const qtdIngUsuario = +prompt("Qual a quantidade de ingressos?");
+const nomeCliente = prompt("Digite seu nome completo:");
+const tipoJogo = prompt("Digite o tipo de jogo: (IN = Internacional/DO = doméstico)").toLowerCase();
+const etapaJogo = prompt("Digite a etapa do jogo: (SF = Semi-Final/DT = Decisão de terceiro lugar/FI = final)").toLocaleLowerCase();
+const categoriaJogo = +prompt("Digite a categoria do jogo:");
+const qtdIng = +prompt("Digite a quantidade de ingressos:");
 
-function sistemaDeIng(nome, tipoJogo, etapaJogo, categoria, qtdIng) {
-    let valorIngJogo;
-    let valorTotalJogo;
-    let tipoJogoParaImpressao; // variável para armazenar string do tipo do jogo sem abreviação.
-    let etapaJogoParaImpressao; // variável para armazenar string da etapa do jogo sem abreviação.
-    let erro = alert("ERRO! Favor preencher as informações corretamente.");
-
-    if(etapaJogoUsuario == "sf") {
-        etapaJogoParaImpressao = "Semi-Final";
-        switch(categoria) {
-            case 1:
-                valorIngJogo = 1320;
-                break;
-            case 2:
-                valorIngJogo = 880;
-                break;
-            case 3:
-                valorIngJogo = 550;
-                break;
-            case 4:
-                valorIngJogo = 220;
-                break;
-            default:
-                valorIngJogo = null;
-                return erro;
-        }
-    } else if(etapaJogoUsuario == "dt") {
-        etapaJogoParaImpressao = "Disputa por terceiro lugar";
-        switch(categoria) {
-            case 1:
-                valorIngJogo = 660;
-                break;
-            case 2:
-                valorIngJogo = 440;
-                break;
-            case 3:
-                valorIngJogo = 330;
-                break;
-            case 4:
-                valorIngJogo = 170;
-                break;
-            default:
-                valorIngJogo = null;
-                return erro;
-        }
-    } else if(etapaJogoUsuario == "fi") {
-        etapaJogoParaImpressao = "Final";
-        switch(categoria) {
-            case 1:
-                valorIngJogo = 1980;
-                break;
-            case 2:
-                valorIngJogo = 1320;
-                break;
-            case 3:
-                valorIngJogo = 880;
-                break;
-            case 4:
-                valorIngJogo = 330;
-                break;
-            default:
-                valorIngJogo = null;
-                return erro;
-        }
-    } else
-        return erro;
-
-    if(tipoJogoUsuario == "do") {
-        valorTotalJogo = valorIngJogo * qtdIng;
-        tipoJogoParaImpressao = "Doméstico";
-    } else if(tipoJogoUsuario == "in") {
-        valorTotalJogo = (valorIngJogo * qtdIng) * 4.10;
-        tipoJogoParaImpressao = "Internacional";
-    } else
-        return erro;
-
-    console.log("---Dados da compra---"); // Nesta linha começa a impressão das informações;
-    console.log(`Nome do cliente: ${nomeUsuario}`);
-    console.log(`Tipo do jogo: ${tipoJogoParaImpressao}`);
-    console.log(`Etapa do jogo: ${etapaJogoParaImpressao}`)
-    console.log(`Categoria: ${categoriaUsuario}`);
-    console.log(`Quantidade de ingressos: ${qtdIngUsuario} ingresso(s)`);
-    console.log("---Valores---");
-    if(tipoJogoUsuario) {
-        console.log(`Valor do ingresso: R$ ${valorIngJogo}`);
-        console.log(`Valor total: R$ ${valorTotalJogo}`);
-    } else {
-        console.log(`Valor do ingresso: $US ${valorIngJogo * 4.10}`);
-        console.log(`Valor total: U$ ${valorTotalJogo}`);
-    }
+const dadosJogo = {
+    nome: nomeCliente,
+    tipo: tipoJogo,
+    etapa: etapaJogo,
+    categoria: categoriaJogo,
+    quantidade: qtdIng,
+    valor: 0,
+    valorTotal: 0
 }
 
-sistemaDeIng(nomeUsuario, tipoJogoUsuario, etapaJogoUsuario, categoriaUsuario, qtdIngUsuario);
+function sistemaDeIng(nome, tipo, etapa, categoria, qtdIng) {
+    // let error = alert("ERRO! Favor preencher os dados corretamente."); - Não consegui fazer a mensagem de erro funcionar corretamente por enquanto, então deixei comentada.
+
+    if(dadosJogo.etapa == "sf") {
+        dadosJogo.etapa = "Semi-Final";
+        switch(dadosJogo.categoria) {
+            case 1:
+                dadosJogo.valor = 1320;
+                break;
+            case 2:
+                dadosJogo.valor = 880;
+                break;
+            case 3:
+                dadosJogo.valor = 550;
+                break;
+            case 4:
+                dadosJogo.valor = 220;
+                break;
+            default:
+                dadosJogo.valor = null;
+                // return error;
+        }
+    } else if(dadosJogo.etapa == "dt") {
+        dadosJogo.etapa = "Disputa por terceiro lugar";
+        switch(dadosJogo.categoria) {
+            case 1:
+                dadosJogo.valor = 660;
+                break;
+            case 2:
+                dadosJogo.valor = 440;
+                break;
+            case 3:
+                dadosJogo.valor = 330;
+                break;
+            case 4:
+                dadosJogo.valor = 170;
+                break;
+            default:
+                dadosJogo.valor = null;
+                // return error;
+        }
+    } else if(dadosJogo.etapa == "fi") {
+        dadosJogo.etapa = "Final";
+        switch(dadosJogo.categoria) {
+            case 1:
+                dadosJogo.valor = 1980;
+                break;
+            case 2:
+                dadosJogo.valor = 1320;
+                break;
+            case 3:
+                dadosJogo.valor = 880;
+                break;
+            case 4:
+                dadosJogo.valor = 330;
+                break;
+            default:
+                dadosJogo.valor = null;
+                // return error;
+        }
+    } else {
+        dadosJogo.etapa == null;
+        // return error;
+    }
+
+    if(dadosJogo.tipo == "do") {
+        dadosJogo.tipo = "Nacional";
+        dadosJogo.valorTotal = `Valor total: R$ ${dadosJogo.valor * dadosJogo.quantidade}`;
+        dadosJogo.valor = `Valor do ingresso: R$ ${dadosJogo.valor}`;
+    } else if(dadosJogo.tipo == "in") {
+        dadosJogo.tipo = "Internacional";
+        dadosJogo.valorTotal = `Valor total: U$${((dadosJogo.valor * 4.10) * dadosJogo.quantidade)}`;
+        dadosJogo.valor = `Valor do ingresso: U$${dadosJogo.valor * 4.10}`;
+    } else {
+        dadosJogo.valor = null;
+    }
+
+    console.log("---Dados da Compra---");
+    console.log(`Nome do cliente: ${dadosJogo.nome}`);
+    console.log(`Tipo do jogo: ${dadosJogo.tipo}`);
+    console.log(`Etapa do jogo: ${dadosJogo.etapa}`);
+    console.log(`Categoria: ${dadosJogo.categoria}`);
+    console.log(`Quantidade de ingressos: ${dadosJogo.quantidade} ingresso(s)`);
+    console.log("---Valores---");
+    console.log(dadosJogo.valor);
+    console.log(dadosJogo.valorTotal);
+}
+
+sistemaDeIng(dadosJogo.nome, dadosJogo.tipo, dadosJogo.etapa, dadosJogo.categoria, dadosJogo.quantidade);
