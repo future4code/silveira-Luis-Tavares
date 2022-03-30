d90cbce604230aa4392f9a6d28f67b529d7338e7
luissfmt <luis_melotavares@hotmail.com>
2022-03-30 11:10:26 -0300

    Exercício 100% finalizado

--- modulo2/ciclo-vida-todo/src/App.js
+++ modulo2/ciclo-vida-todo/src/App.js
@@ -24,43 +24,59 @@ const InputsContainer = styled.div`
 
 class App extends React.Component {
     state = {
-      tarefas: [
-        {
-          id: Date.now(),
-          texto: 'Tarefas Faculdade',
-          completa: false
-        },
-        {
-          id: Date.now(),
-          texto: 'Exercícios Labenu',
-          completa: true
-        }],
+      tarefas: [],
       inputValue: '',
       filtro: ''
     }
 
   componentDidUpdate() {
-
+    localStorage.setItem("tarefas", JSON.stringify(this.state.tarefas));
   };
 
   componentDidMount() {
-
+    this.setState({
+      tarefas: JSON.parse(localStorage.getItem('tarefas'))
+    })
   };
 
   onChangeInput = (event) => {
-
+    this.setState({inputValue: event.target.value});
   }
 
   criaTarefa = () => {
+    const novaTarefa = {
+      id: Date.now(),
+      texto: this.state.inputValue,
+      completa: false
+    }
 
+    const tarefasAtualizadas = [...this.state.tarefas, novaTarefa];
+
+    this.setState({
+      tarefas: tarefasAtualizadas,
+      inputValue: '',
+    });
   }
 
   selectTarefa = (id) => {
+    const alterarEstadoTarefa = this.state.tarefas.map((tarefa) => {
+      if(id === tarefa.id) {
+        const novaListaTarefas = {
+          ...tarefa, 
+          completa: !tarefa.completa
+        }
 
+        return novaListaTarefas;
+      } else {
+        return tarefa;
+      }
+    })
+
+    this.setState({tarefas: alterarEstadoTarefa});
   }
 
   onChangeFilter = (event) => {
-
+    this.setState({filtro: event.target.value});
   }
 
   render() {
@@ -92,10 +108,12 @@ class App extends React.Component {
             <option value="completas">Completas</option>
           </select>
         </InputsContainer>
+
         <TarefaList>
-          {listaFiltrada.map(tarefa => {
+          {listaFiltrada.map((tarefa, key) => {
             return (
               <Tarefa
+              key={key}
                 completa={tarefa.completa}
                 onClick={() => this.selectTarefa(tarefa.id)}
               >
