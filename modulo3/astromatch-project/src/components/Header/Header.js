9f93e5b3a393b15962e7e5924572313dd4ecc829
luissfmt <luis_melotavares@hotmail.com>
2022-04-22 12:48:20 -0300

    fixing broke icons

--- modulo3/astromatch-project/src/components/Header/Header.js
+++ modulo3/astromatch-project/src/components/Header/Header.js
@@ -1,26 +1,30 @@
 import React from "react";
 import { HeaderMainPage, HeaderMatchesPage } from "../../stylesHeader";
 
+import previousIcon from "../../assets/imgs/backward.png";
+import astromatchLogo from "../../assets/imgs/tinder.png";
+import matchesIcon from "../../assets/imgs/matches.png"
+
 export function Header(props) {
     return (
         <>            
             {props.currentPage == "main" ? (
                 <HeaderMainPage>
                     <div>
-                        <img src="https://cdn-icons.flaticon.com/png/512/4423/premium/4423669.png?token=exp=1650635236~hmac=2568f44e63820866350079c75da457e6" />
+                        <img src={astromatchLogo} />
                         <h1>astromatch</h1>
                     </div>
 
-                    <img onClick={() => props.changePage("matches")} src="https://cdn-icons-png.flaticon.com/512/7111/7111090.png" />
+                    <img onClick={() => props.changePage("matches")} src={matchesIcon} />
                 </HeaderMainPage>
             ) : (
                 <HeaderMatchesPage>
                     <div>
-                        <img src="https://cdn-icons.flaticon.com/png/512/4423/premium/4423669.png?token=exp=1650635236~hmac=2568f44e63820866350079c75da457e6" />
+                        <img src={astromatchLogo} />
                         <h1>astromatch</h1>
                     </div>
                     
-                    <img onClick={() => props.changePage("main")} src="https://cdn-icons.flaticon.com/png/512/5464/premium/5464039.png?token=exp=1650640303~hmac=a69032970491c06c7415f2d554fb0df5" />
+                    <img onClick={() => props.changePage("main")} src={previousIcon} />
                 </HeaderMatchesPage>
             )}
         </>
