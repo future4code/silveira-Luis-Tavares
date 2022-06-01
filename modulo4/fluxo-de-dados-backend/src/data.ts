34b6bd1d6ec7d5811b2320e74c12d699d0bf3e3c
luissfmt <luis_melotavares@hotmail.com>
2022-06-01 18:11:23 -0300

    Exercícíos de fluxo de dados

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,11 @@
+type Product = {
+    id: string,
+    name: string,
+    price: number
+};
+
+export const products: Array<Product> = [
+    {id: "8740527601934", name: "Monitor", price: 2500},
+    {id: "1297859486012", name: "Mouse", price: 200},
+    {id: "3274523347487", name: "Teclado", price: 450}
+];
