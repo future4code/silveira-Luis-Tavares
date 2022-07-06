fc50e865dba0bde50ab10b8168b5476d2f0ee329
luissfmt <luis_melotavares@hotmail.com>
2022-07-06 12:13:30 -0300

    Finalização dos endpoints mínimos

--- modulo5/projeto-labook/src/index.ts
+++ modulo5/projeto-labook/src/index.ts
@@ -7,7 +7,11 @@ import { HashManager } from "./services/HashManager";
 import { UserDatabase } from "./data/UserDatabase";
 import { Authenticator } from "./services/Authenticator";
 import { UserController } from "./controller/UserController";
+import { PostBusiness } from "./business/PostBusiness";
+import { PostDatabase } from "./data/PostDatabase";
+import { PostController } from "./controller/PostController";
 
+// ----- INSTANCIAS DA CLASSE USER
 const userBusiness = new UserBusiness(
     new IdGenerator(),
     new HashManager(),
@@ -19,8 +23,25 @@ const userController = new UserController(
     userBusiness
 );
 
+// ----- INSTANCIAS DA CLASSE POST
+const postBusiness = new PostBusiness(
+    new Authenticator(),
+    new IdGenerator(),
+    new PostDatabase()
+);
+
+const postController = new PostController(
+    postBusiness
+);
+
 // ----- SIGNUP -----
 app.post("/user/signup", userController.signUp);
 
 // ----- LOGIN -----
 app.post("/user/login", userController.logIn);
+
+// ----- POST CREATION -----
+app.post("/post/create", postController.createPost);
+
+// ----- GET POST BY ID -----
+app.get("/post/:id", postController.getPostById);
